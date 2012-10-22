d88360fc42116b41202fdf19c99049ad4f638409
Chris Newton <chris@chrisnewton.com>
2012-10-22 12:26:31 -0500

    First Push

---
+++ README.md
@@ -0,0 +1,14 @@
+extension-credits-add
+=============================
+
+This extension adds credits to a customer credit account (When CT Credits is installed) upon completion of a successful purchase with CartThrob. You can adjust the conversion rate (set to 100 by default) in the extension settings
+
+This is a standard EE extension which is installed & configured like other extensions: 
+Installation: move file to system > expressionengine > third_party 
+Follow additional installation instructions here: 
+http://expressionengine.com/user_guide/cp/add-ons/extension_manager.html
+
+
+
+
+This add-on is provided as-is at no cost with no warranty expressed or implied. Support is not included. 
---
+++ cartthrob_credits_add/ext.cartthrob_credits_add.php
@@ -0,0 +1,121 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+/*
+// to use functions from mod.cartthrob.php file
+require_once PATH_THIRD.'cartthrob/mod.cartthrob.php';
+// then instatiate it: $mod = new Cartthrob;
+// call methods as needed $mod->method(); 
+*/
+
+class Cartthrob_credits_add_ext
+{
+	public $settings = array();
+	public $name = 'CartThrob - Adds credits';
+	public $version = '1.0.0';
+	public $description = 'Adds credits to a members account based on cart total. Update the conversion rate in the extension settings. Defaults to 100.';
+	public $settings_exist = 'n';
+	public $docs_url = 'http://barrettnewton.com';
+ 	protected $EE;
+
+	public function __construct($settings = '')
+	{
+		$this->EE =& get_instance();
+		
+		$this->settings = $settings;
+	}
+
+	public function activate_extension()
+	{
+		$this->settings = array(
+	        'credits_per_dollar'   => "100",
+	    );
+		$this->EE->db->insert(
+			'extensions',
+			array(
+				'class' => __CLASS__,
+				'method' => 'cartthrob_on_authorize',
+				'hook' 	=> 'cartthrob_reports',
+				'settings' => '',
+				'priority' => 10,
+				'version' => $this->version,
+				'enabled' => 'y'
+			)
+		);
+	}
+	
+	public function update_extension($current='')
+	{
+		if ($current == '' OR $current == $this->version)
+		{
+			return FALSE;
+		}
+
+		$this->EE->db->update(
+			'extensions',
+			array('version' => $this->version),
+			array('class' => __CLASS__)
+		);
+	}
+
+	public function disable_extension()
+	{
+		$this->EE->db->delete('extensions', array('class' => __CLASS__));
+	}
+	function settings()
+	{
+	    $settings = array();
+
+ 	    $settings['credits_per_dollar']      = array('i', '', "100");
+
+	    return $settings;
+	}
+	public function cartthrob_on_authorize()
+	{
+		if ($this->EE->db->table_exists('exp_cartthrob_cartthrob_credits')) 
+		{   
+			$this->EE->load->helper('data_formatting'); 
+			
+			$credits_per_dollar = sanitize_number($this->settings['credits_per_dollar']); 
+ 			
+			$total = $this->EE->cartthrob->order('total'); 
+			
+			// if credits have been added to the basket, we'll remove them from the total
+			foreach ( $this->EE->cartthrob->cart->order('items') as $item )
+			{
+				// Calculate the credits.
+				if (!empty($item['meta']['credits']) && is_bool($item['meta']['credits']))
+				{
+					// applying price to the conversion rate
+					$total -= ($item['price'] * $item['quantity'] );
+				}
+				elseif(!empty($item['meta']['credits']))
+				{
+					// otherwise just adding the preset credit value * quantity
+					$total -=($item['meta']['credits'] * $item['quantity'] ) ; 
+				}
+			}
+				
+			$credits = number_format( (double) (($total * $credits_per_dollar)) , 2, ".","");
+
+			$this->EE->load->model('generic_model');
+			$model = new Generic_model($table_name = 'cartthrob_credits_credits');
+
+			$member_data = (array) $model->read($id=NULL,$order_by=NULL,$order_direction='asc',$field_name="member_id",$this->EE->cartthrob->cart->order('member_id'),$limit="1"); 	
+
+			if (!empty($member_data) )
+			{
+				foreach($member_data as $key => $update_data)
+				{
+					$update_data['credits'] +=  $credits;
+					$model->update($update_data['id'], $update_data);
+				}
+			}
+			// no member data. create some
+			else
+			{
+				$model->create(array('member_id' => $this->EE->cartthrob->cart->order('member_id'), 'credits' => $credits));
+			}
+		}
+ 	}
+}
+//END CLASS
